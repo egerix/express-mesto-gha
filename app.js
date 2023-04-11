ae2330ae567408542de8ff4864a80921cc6ca964
Egor Ivanov <egerix@gmail.com>
2023-04-11 15:16:18 +0200

    homework 13: fix helmet

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const limiter = rateLimit({
 const app = express();
 
 app.use(limiter);
-app.use(helmet);
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
